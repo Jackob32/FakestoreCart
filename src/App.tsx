ec75d50a6a81bc242014efe632f3485d5dbe0c25
Jakub <Jakub.trhlik@gmail.com>
2023-07-27 08:12:02 +0200

    Update ProductTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import ProductTable from "./ProductTable"
+import {useEffect, useState} from "react";
+import {Product} from "./types";
 
 export const App = () => {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then((data) => setProducts(data))
+    }, []);
+
     return <>
-        <ProductTable />
+        <ProductTable products={products}/>
     </>;
 }
 
